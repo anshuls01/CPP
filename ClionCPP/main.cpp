b11bbc0218f899d22b7204f79c49d055583cbecb
anshul.singhal <singhal.s01@gmail.com>
2024-05-16 07:24:12 +0530

    simple program

--- ClionCPP/main.cpp
+++ ClionCPP/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
-
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Convert from fahrenheit to Celsius" << endl;
+    cout << "Insert temprature in Fahrenheit: ";
+    double Fahrenheit = 0.0;
+    cin >> Fahrenheit;
+   // double Celsius = (Fahrenheit - 32)/1.8;
+    cout << "Temp in Celsius : " <<  (Fahrenheit - 32)/1.8;;
+    cout << "Hello, World!" << endl;
+    
+    // int a = 1;
+    // int b = 2;
+    // cout<<a<<endl;
+    // cout<<b;
+    // //swapIntegerValue(&a, &b);
+    // cout<<a;
+    // cout<<b;
     return 0;
+}
+
+void swapIntegerValue(int *a, int *b) {
+    int *c = b;
+    b = a;
+    a = c;
 }
